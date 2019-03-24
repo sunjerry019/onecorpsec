cfa4da52623f324da4b0322158e624409449e84e
Sun Yudong <sunjerry019@gmail.com>
2019-03-24 17:47:57 +0800

    add some wsgi preparation code

--- dashboard/mailer.py
+++ dashboard/mailer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# https://www.stavros.io/posts/standalone-django-scripts-definitive-guide/
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
 from htmlemailer import send_mail
 
 send_mail(
