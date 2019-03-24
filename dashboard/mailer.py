36a136d5dde04ddc1c73c8819ee8543bc084630d
Sun Yudong <sunjerry019@gmail.com>
2019-03-24 18:01:11 +0800

    try without htmlmailer

--- dashboard/mailer.py
+++ dashboard/mailer.py
@@ -4,12 +4,25 @@
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-from htmlemailer import send_mail
+# from htmlemailer import send_mail
+#
+# send_mail(
+# 	"htmlemailer/example",
+# 	"My Site <yudong@onecorpsec.com>",
+# 	["sunjerry019@gmail.com"],
+# 	{
+# 		"my_message": "Hello & good day to you!"
+# 	})
 
-send_mail(
-	"htmlemailer/example",
-	"My Site <yudong@onecorpsec.com>",
-	["sunjerry019@gmail.com"],
-	{
-		"my_message": "Hello & good day to you!"
-	})
+from django.core.mail import EmailMultiAlternatives
+import settings
+
+def send_mail():
+ from_email = settings.EMAIL_HOST_USER
+ to_email = ['sunjerry019@gmail.com']
+ subject = "Any text"
+ body = "Hello world"
+ msg = EmailMultiAlternatives(subject, body, from_email, to_email)
+ msg.send()
+
+send_mail()
