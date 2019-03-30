6c5c52cfcedb1ee1d926d827f4e5aace62c2e484
Sun Yudong <sunjerry019@gmail.com>
2019-03-31 00:51:01 +0800

    navbar proof of concept

--- dashboard/manager/views.py
+++ dashboard/manager/views.py
@@ -8,7 +8,10 @@ from .models import getTable
 
 def render_Home(request):
     t = get_template('home.html')
-    _model = getTable(request.user.username)
-    rows = _model.objects.all()
-    html = t.render({ 'hostname': "OneCorpSec" , 'user': request.user, 'rows': rows })
+    if request.user.is_authenticated:
+        _model = getTable(request.user.username)
+        rows = _model.objects.all()
+    else:
+        rows = []
+    html = t.render({ 'hostname': "OneCorpSec" , 'user': request.user, 'rows': rows, 'rowCount': len(rows) })
     return HttpResponse(html)
