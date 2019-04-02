76bca6daccb7ba548bb240d640e79b9ab138977b
Sun Yudong <sunjerry019@gmail.com>
2019-04-02 18:21:27 +0800

    Move script out and set up for handling of updating database

--- dashboard/manager/views.py
+++ dashboard/manager/views.py
@@ -5,13 +5,14 @@ from django.template.loader import get_template
 from django.core.paginator import Paginator
 from django.http import HttpResponse
 from .models import getTable
+from django.views.decorators.csrf import ensure_csrf_cookie
 
-
+@ensure_csrf_cookie
 def render_Home(request, page = 1):
     t = get_template('home.html')
     if request.user.is_authenticated:
         _model = getTable(request.user.username)
-        all_rows = _model.objects.all().order_by('coyregno')
+        all_rows = _model.objects.all().order_by('coyname')
 
         numitems = request.session['enum'] if 'enum' in request.session else 25
         numitems = request.GET.get('enum') or numitems
@@ -33,3 +34,8 @@ def render_Home(request, page = 1):
         'override_base' : override_base
     })
     return HttpResponse(html)
+
+
+def updateDatabase(request):
+    # Trigger the checker here
+    pass
