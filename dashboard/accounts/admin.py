11380c23844f8e82307a903eaaf15da2eb79bedf
Sun Yudong <sunjerry019@gmail.com>
2019-04-29 06:55:18 +0800

    add optional group stuff that actually doesnt work and im too lazy to fix it

--- dashboard/accounts/admin.py
+++ dashboard/accounts/admin.py
@@ -1,4 +1,5 @@
 from .forms import DatabaseUserCreationForm, DatabaseUserChangeForm
+# from .forms import GroupAdminForm
 from .models import DatabaseUser
 
 from django.contrib import admin
@@ -11,5 +12,29 @@ class DatabaseUserAdmin(UserAdmin):
     form = DatabaseUserChangeForm
     model = DatabaseUser
     list_display = ['email', 'username', 'name', 'sign_off_name']
+    # https://stackoverflow.com/a/24438757/3211506
+    fieldsets = (
+            (None, {'fields': ('username', 'password',)}),
+            ('Personal info', {'fields': ('name', 'first_name', 'last_name', 'sign_off_name', 'email', 'reply_to', 'date_joined', 'last_login',)}),
+            ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions',)}),
+        )
+    search_fields = ('id', 'username', 'email', 'name', 'first_name', 'last_name',)
+    ordering = ('username',)
+
+    # If you ever want to add groups to the list_display
+    # https://stackoverflow.com/a/31195602/3211506
 
 admin.site.register(DatabaseUser, DatabaseUserAdmin)
+
+# # Unregister the original Group admin.
+# admin.site.unregister(Group)
+#
+# # Create a new Group admin.
+# class GroupAdmin(admin.ModelAdmin):
+#     # Use our custom form.
+#     form = GroupAdminForm
+#     # Filter permissions horizontal as well.
+#     filter_horizontal = ['permissions']
+#
+# # Register the new Group ModelAdmin.
+# admin.site.register(Group, GroupAdmin)
