2bbe276e588b85cfd2fd1b3f551a82d7bc616e59
Sun Yudong <sunjerry019@gmail.com>
2019-04-29 07:15:49 +0800

    sign up as view DONE UWU

--- dashboard/accounts/views.py
+++ dashboard/accounts/views.py
@@ -1,12 +1,15 @@
 # from django.shortcuts import render
 
 # Create your views here.
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from .forms import DatabaseUserCreationForm
+from .models import DatabaseUser
 
+# from django.contrib.auth.forms import UserCreationForm
+# from django import forms
 
 class SignUp(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = DatabaseUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
