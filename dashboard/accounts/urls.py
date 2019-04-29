51ce233344c6186a14564430facbb166a1253168
Sun Yudong <sunjerry019@gmail.com>
2019-04-29 08:15:03 +0800

    redirect accounts to admin

--- dashboard/accounts/urls.py
+++ dashboard/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
+from django.urls import reverse_lazy
 
 from . import views
 
 urlpatterns = [
+    path('', RedirectView.as_view(url=reverse_lazy('admin:index'), permanent=False)), # https://stackoverflow.com/a/9093623/3211506
     path('signup/', views.SignUp.as_view(), name='signup'),
 ]
